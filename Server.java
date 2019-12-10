04c4e27fd7dee4ca73d0e930050a3746bb95f765
daniel81181 <51387056+daniel81181@users.noreply.github.com>
2019-12-10 05:58:23 -0600

    Update Server.java

--- Server.java
+++ Server.java
@@ -1,25 +1,32 @@
+package socket;
+
 import java.net.*;
 import java.io.*;
-public class Server{
-public static void main(String args[])throws Exception{
-ServerSocket ss=new ServerSocket(3333);
-Socket s=ss.accept();
-DataInputStream din=new DataInputStream(s.getInputStream());
-DataOutputStream dout=new DataOutputStream(s.getOutputStream());
-BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
 
-String str="",str2="";
-while(!str.equals("stop")){
-str=din.readUTF();
-System.out.println("client says: "+str);
-str2=br.readLine();
-dout.writeUTF(str2);
-dout.flush();
-}
-  		//Socket enviaDestinatario=new Socket("localhost",5555);
+public class Server {
+
+	public static void main(String args[]) throws Exception {
+		ServerSocket ss = new ServerSocket(3333);
+		Socket s = ss.accept();
+		DataInputStream din = new DataInputStream(s.getInputStream());
+		DataOutputStream dout = new DataOutputStream(s.getOutputStream());
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		String str = "", str2 = "";
+		while (!str.equals("stop")) {
+			str = din.readUTF();
+			System.out.println("client says: " + str);
+			str2 = br.readLine();
+			dout.writeUTF(str2);
+			dout.flush();
+			
+		//Socket enviaDestinatario=new Socket("localhost",5555);
 		
 		//DataOutputStream dou1=new DataOutputStream(enviaDestinatario.getOutputStream());
-din.close();
-s.close();
-ss.close();
-}}
+		}
+		
+		din.close();
+		s.close();
+		ss.close();
+	}
+}
